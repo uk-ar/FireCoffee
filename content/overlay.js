b34df73d66550acbf3f9ad358954a86b7dc77c45
yuuki arisawa <yuuki.ari@gmail.com>
2011-07-28 20:10:05 +0900

    can modify code in chromebug

---
+++ content/overlay.js
@@ -0,0 +1,11 @@
+var TagHelper = {
+  generateTags: function() {
+    alert("hoge");
+  },
+};
+//in chrome bug Filter taghelper File overlay.js
+// var TagHelper = {
+//   generateTags: function() {
+//     alert(",ll");
+//   },
+// };
